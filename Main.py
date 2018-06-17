15fef6846b13c2931664c2dee3ffab57a52ac5f4
siddhantxshirguppe <siddhantxshirguppe@gmail.com>
2018-06-17 20:34:37 +0530

    Initial commit

---
+++ Main.py
@@ -0,0 +1,174 @@
+from tkinter.filedialog import askopenfilename
+import tkinter
+
+stre="emails.csv"
+searchkeywords=""
+
+def open1():
+    filename = askopenfilename()
+    global stre
+    print(filename)
+    stre = str(filename)
+    global searchkeywords
+    searchkeywords = e1.get()
+    print(searchkeywords)
+
+def quit(self):
+
+    self.root.destroy()
+
+
+def open2():
+    global searchkeywords
+    searchkeywords = e1.get()
+    print(searchkeywords)
+    top.quit()
+
+
+
+top = tkinter.Tk()
+E=tkinter.Label(top,text="enter the keywords seperated by commas:eg(python,opencv,code)->")
+B = tkinter.Button(top, text ="SUBMIT ", command = top.quit)
+C = tkinter.Button(top, text ="UPLOAD", command = open1)
+A = tkinter.Label(top, text ="Upload CSV files in which contains rows in the following format:\n"
+                             "<date in dd/mm/yy>$<emailid>$<mail content>\n"
+                             "ex: 30/09/17$shubhi.prakash03@gmail.com$will be doing it by tomorrow \n"
+                             "Output will be displayed in the terminal\n"
+                  )
+D = tkinter.Button(top, text ="Open default", command = open2)
+A.grid(row=0)
+C.grid(row=1)
+B.grid(row=2)
+D.grid(row=3 )
+E.grid(row=4,column=0)
+e1=tkinter.Entry(top)
+e1.insert(0, "Python")
+e1.grid(row=4,column=1,columnspan=28)
+top.mainloop()
+
+import csv
+import time
+from time import mktime
+from datetime import datetime
+
+
+def parse(text):
+    for fmt in ('%Y-%m-%d', '%d/%m/%y',''):
+        try:
+            return datetime.strptime(text, fmt)
+        except ValueError:
+            pass
+    raise ValueError('no valid date format found')
+
+
+
+filename= stre.replace("\\", "/")
+
+fields = []
+rows = []
+
+
+with open(filename, 'r') as csvfile:
+
+    csvreader = csv.reader(csvfile)
+
+
+    fields = next(csvreader)
+
+
+    for row in csvreader:
+        rows.append(row)
+
+
+    print("Total no. of rows: %d \n" % (csvreader.line_num))
+
+
+dic=[]
+listdate=[]
+listmail=[]
+listmatter=[]
+
+for row in rows[:csvreader.line_num]:
+
+    for col in row:
+        #print(col)
+        dic.append({"date":str(str(col).split('$')[0]),"name":str(str(col).split("$")[1]),"matter":str(str(col).split("$")[2])})
+
+
+    #print('\n')
+
+
+myset=set()
+for i in dic:
+   myset.add(str(i["name"]))
+
+print("THE UNIQUE EMAIL  ADDRESSESS ARE: \n ")
+for i in myset:
+    print(i+"\n")
+
+print("********************\n")
+
+print("THE LAST DATE OF CONVERSATION FOR THE EMAILS ARE:\n")
+
+searcharr=searchkeywords.split(",")
+print(searcharr)
+currentdate=datetime.today().strftime('%d/%m/%y')
+
+
+for i in myset:
+    min = "20/12/01"
+    maxdate = time.strptime(min, "%d/%m/%y")
+    for j in range(len(dic)):
+        if(i==dic[j]["name"]):
+
+            try:
+             if(maxdate<time.strptime(dic[j]["date"],"%d/%m/%y")):
+                maxdate = time.strptime(dic[j]["date"],"%d/%m/%y")
+            except:
+             pass
+    latestdate = datetime.fromtimestamp(mktime(maxdate)).strftime("%d/%m/%Y")
+    latestdateobj= datetime.fromtimestamp(mktime(maxdate))
+    print("--------------------")
+    print("email:",i)
+    print("last date of conservation: ", latestdate)
+
+
+    now = datetime.today()
+    delta = now - latestdateobj
+    print("Days elapsed from last date: ",delta.days)
+
+
+print("--------------------")
+
+print("KEYWORD SEARCH")
+flag=True
+for a in dic:
+
+    for b in searcharr:
+        if( b in a["matter"] ):
+            flag=False
+            print("Keyword \""+b+"\" found in "+a["name"]+" dated:"+a["date"])
+
+if(flag==True):
+    print("no mails contains the required keywords")
+
+
+
+
+#-------------eof---------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
